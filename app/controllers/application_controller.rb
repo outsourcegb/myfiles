2f197f6e01f8e7d0103c06d99e633768a461ffc5
Shashi <outsource.gb@gmail.com>
2022-01-23 21:23:21 +0530

    Notification and other minore changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :config_devise_params, if: :devise_controller?
+
+  protected
+
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[
+                                        full_name
+                                        email
+                                        password
+                                        password_confirmation
+                                      ])
+  end
 end
