a1e13baca27347df27d545538104189707da19e6
Shashi <outsource.gb@gmail.com>
2022-01-23 20:31:38 +0530

    Added simple_for, devise, rspec, factory_bot, annotate, letter_opener_web gems and changed files for their basic installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'site/index'
+  devise_for :users
+  # get 'site/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "site#index"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
